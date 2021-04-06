03ec189b10791ed73fae4ef72cc181efbeffca8e
Abdul Muhammad <abdulmuhammad331@gmail.com>
2021-04-06 14:58:27 -0700

    Added icons to navigation bar, started fragment code in MainActivity

--- app/src/main/java/com/example/animehub/MainActivity.java
+++ app/src/main/java/com/example/animehub/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.animehub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+//TODO: PLEASE HELP ME FIX THIS
+    /*  bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        Toast.makeText(MainActivity.this, "Home!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_discussion:
+                        Toast.makeText(MainActivity.this, "Discussion!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        Toast.makeText(MainActivity.this, "Profile!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });*/
     }
 }
